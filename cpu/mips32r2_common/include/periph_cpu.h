2a11fb584615a3c9bbe461efea6065925d97de49
Neil Jones <neil.jones@imgtec.com>
2016-11-04 12:19:14 +0000

    cpu: mips: Add support for mips32r2
    cpu/mips32r2_common adds base architecture support for mips32r2 cores it can be
    built in it own right as a 'CPU', but is dependant on a bootloader (like
    u-boot) to have bootstrapped the system, this has been tested on a 'malta'
    FPGA system (BOARD=mips-malta) with various mips32r2 compliant cores
    (interAptiv, P5600, etc).

---
+++ cpu/mips32r2_common/include/periph_cpu.h
@@ -0,0 +1,9 @@
+/* This file must exist to get timer code to build */
+
+/* No peripherals I/O via JTAG or Bootloader using UHI */
+
+/*
+ * Note mips32r2_common can be selected in its own right as a CPU
+ * for testing on PFGA systems (like BOARD=mips-malta) with limited
+ * or no peripherals
+ */
