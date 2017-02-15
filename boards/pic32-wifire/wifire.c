423aa52376354805352f3b22264e522deff9e17c
Neil Jones <neil.jones@imgtec.com>
2017-02-15 10:38:29 +0000

    SQUASH: boards/pic32-wifire: Address review comments.

--- boards/pic32-wifire/wifire.c
+++ boards/pic32-wifire/wifire.c
@@ -1,10 +1,11 @@
 /*
- * Copyright 2016, Imagination Technologies Limited and/or its
+ * Copyright(C) 2016,2017, Imagination Technologies Limited and/or its
  *                 affiliated group companies.
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
  * directory for more details.
+ *
  */
 
 #include <stdio.h>
